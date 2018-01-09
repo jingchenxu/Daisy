8c24f44bbc0c8cd22f19d5b38dfb658efeff7cc3
jingchenxu <jingchenxu2015@gmail.com>
2018-01-09 23:13:14 +0800

    init

---
+++ src/main/java/org/deepwater/daisy/DaisyApplication.java
@@ -0,0 +1,12 @@
+package org.deepwater.daisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisyApplication.class, args);
+	}
+}
---
+++ src/test/java/org/deepwater/daisy/DaisyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.deepwater.daisy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaisyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
