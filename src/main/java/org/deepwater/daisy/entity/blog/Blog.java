a193b484bd6191edb322a8e21bbfdac7fb4438cf
jingchenxu <jingchenxu2015@gmail.com>
2018-01-18 19:37:56 +0800

    (common)注释了代码生成插件配置

--- src/main/java/org/deepwater/daisy/entity/blog/Blog.java
+++ src/main/java/org/deepwater/daisy/entity/blog/Blog.java
@@ -1,8 +1,10 @@
 package org.deepwater.daisy.entity.blog;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
-public class Blog {
+public class Blog implements Serializable {
     private Integer blogId;
 
     private String blogTitle;
@@ -24,6 +26,12 @@ public class Blog {
     private String blogNumber;
 
     private String blogContent;
+
+    private List<String> tags;
+
+    private int pageNo;
+
+    private int pageSize;
 
     public Integer getBlogId() {
         return blogId;
@@ -111,5 +119,49 @@ public class Blog {
 
     public void setBlogContent(String blogContent) {
         this.blogContent = blogContent == null ? null : blogContent.trim();
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "blogId=" + blogId +
+                ", blogTitle='" + blogTitle + '\'' +
+                ", blogAuthor='" + blogAuthor + '\'' +
+                ", blogPublishtime=" + blogPublishtime +
+                ", blogBannerurl='" + blogBannerurl + '\'' +
+                ", blogIntroduction='" + blogIntroduction + '\'' +
+                ", blogStatus='" + blogStatus + '\'' +
+                ", blogType='" + blogType + '\'' +
+                ", blogSubtitle='" + blogSubtitle + '\'' +
+                ", blogNumber='" + blogNumber + '\'' +
+                ", blogContent='" + blogContent + '\'' +
+                ", tags=" + tags +
+                ", pageNo=" + pageNo +
+                ", pageSize=" + pageSize +
+                '}';
     }
 }
--- src/main/java/org/deepwater/daisy/mapper/blog/BlogMapper.java
+++ src/main/java/org/deepwater/daisy/mapper/blog/BlogMapper.java
@@ -1,41 +1,35 @@
 package org.deepwater.daisy.mapper.blog;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.annotations.UpdateProvider;
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.type.JdbcType;
 import org.deepwater.daisy.entity.blog.Blog;
 import org.deepwater.daisy.entity.blog.BlogExample;
 
+@Mapper
 public interface BlogMapper {
     @SelectProvider(type=BlogSqlProvider.class, method="countByExample")
     long countByExample(BlogExample example);
 
     @Delete({
-        "delete from T_DAISY_BLOG",
-        "where blog_id = #{blogId,jdbcType=INTEGER}"
+            "delete from T_DAISY_BLOG",
+            "where blog_id = #{blogId,jdbcType=INTEGER}"
     })
     int deleteByPrimaryKey(Integer blogId);
 
     @Insert({
-        "insert into T_DAISY_BLOG (blog_id, blog_title, ",
-        "blog_author, blog_publishtime, ",
-        "blog_bannerurl, blog_introduction, ",
-        "blog_status, blog_type, ",
-        "blog_subtitle, blog_number, ",
-        "blog_content)",
-        "values (#{blogId,jdbcType=INTEGER}, #{blogTitle,jdbcType=VARCHAR}, ",
-        "#{blogAuthor,jdbcType=VARCHAR}, #{blogPublishtime,jdbcType=DATE}, ",
-        "#{blogBannerurl,jdbcType=VARCHAR}, #{blogIntroduction,jdbcType=VARCHAR}, ",
-        "#{blogStatus,jdbcType=VARCHAR}, #{blogType,jdbcType=VARCHAR}, ",
-        "#{blogSubtitle,jdbcType=VARCHAR}, #{blogNumber,jdbcType=VARCHAR}, ",
-        "#{blogContent,jdbcType=LONGVARCHAR})"
+            "insert into T_DAISY_BLOG (blog_id, blog_title, ",
+            "blog_author, blog_publishtime, ",
+            "blog_bannerurl, blog_introduction, ",
+            "blog_status, blog_type, ",
+            "blog_subtitle, blog_number, ",
+            "blog_content)",
+            "values (#{blogId,jdbcType=INTEGER}, #{blogTitle,jdbcType=VARCHAR}, ",
+            "#{blogAuthor,jdbcType=VARCHAR}, #{blogPublishtime,jdbcType=DATE}, ",
+            "#{blogBannerurl,jdbcType=VARCHAR}, #{blogIntroduction,jdbcType=VARCHAR}, ",
+            "#{blogStatus,jdbcType=VARCHAR}, #{blogType,jdbcType=VARCHAR}, ",
+            "#{blogSubtitle,jdbcType=VARCHAR}, #{blogNumber,jdbcType=VARCHAR}, ",
+            "#{blogContent,jdbcType=LONGVARCHAR})"
     })
     int insert(Blog record);
 
@@ -43,58 +37,101 @@ public interface BlogMapper {
     int insertSelective(Blog record);
 
     @Select({
-        "select",
-        "blog_id, blog_title, blog_author, blog_publishtime, blog_bannerurl, blog_introduction, ",
-        "blog_status, blog_type, blog_subtitle, blog_number, blog_content",
-        "from T_DAISY_BLOG",
-        "where blog_id = #{blogId,jdbcType=INTEGER}"
+            "select",
+            "blog_id, blog_title, blog_author, blog_publishtime, blog_bannerurl, blog_introduction, ",
+            "blog_status, blog_type, blog_subtitle, blog_number, blog_content",
+            "from T_DAISY_BLOG",
+            "where blog_id = #{blogId,jdbcType=INTEGER}"
     })
     @Results({
-        @Result(column="blog_id", property="blogId", jdbcType=JdbcType.INTEGER, id=true),
-        @Result(column="blog_title", property="blogTitle", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_author", property="blogAuthor", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_publishtime", property="blogPublishtime", jdbcType=JdbcType.DATE),
-        @Result(column="blog_bannerurl", property="blogBannerurl", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_introduction", property="blogIntroduction", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_status", property="blogStatus", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_type", property="blogType", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_subtitle", property="blogSubtitle", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_number", property="blogNumber", jdbcType=JdbcType.VARCHAR),
-        @Result(column="blog_content", property="blogContent", jdbcType=JdbcType.LONGVARCHAR)
+            @Result(column="blog_id", property="blogId", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="blog_title", property="blogTitle", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_author", property="blogAuthor", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_publishtime", property="blogPublishtime", jdbcType=JdbcType.DATE),
+            @Result(column="blog_bannerurl", property="blogBannerurl", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_introduction", property="blogIntroduction", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_status", property="blogStatus", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_type", property="blogType", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_subtitle", property="blogSubtitle", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_number", property="blogNumber", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_content", property="blogContent", jdbcType=JdbcType.LONGVARCHAR)
     })
     Blog selectByPrimaryKey(Integer blogId);
+
+    @Select({
+            "select",
+            "blog_id, blog_title, blog_author, blog_publishtime, blog_bannerurl, blog_introduction, ",
+            "blog_status, blog_type, blog_subtitle, blog_number, blog_content",
+            "from T_DAISY_BLOG",
+            "where blog_number = #{blogNumber,jdbcType=INTEGER}"
+    })
+    @Results({
+            @Result(column="blog_id", property="blogId", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="blog_title", property="blogTitle", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_author", property="blogAuthor", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_publishtime", property="blogPublishtime", jdbcType=JdbcType.DATE),
+            @Result(column="blog_bannerurl", property="blogBannerurl", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_introduction", property="blogIntroduction", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_status", property="blogStatus", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_type", property="blogType", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_subtitle", property="blogSubtitle", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_number", property="blogNumber", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_content", property="blogContent", jdbcType=JdbcType.LONGVARCHAR)
+    })
+    Blog selectByBlogNumber(String blogNumber);
+
+    @Select({
+            "select",
+            "*",
+            "from T_DAISY_BLOG WHERE TRUE",
+            "ORDER BY blog_publishtime DESC"
+    })
+    @Results({
+            @Result(column="blog_id", property="blogId", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="blog_title", property="blogTitle", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_author", property="blogAuthor", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_publishtime", property="blogPublishtime", jdbcType=JdbcType.DATE),
+            @Result(column="blog_bannerurl", property="blogBannerurl", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_introduction", property="blogIntroduction", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_status", property="blogStatus", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_type", property="blogType", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_subtitle", property="blogSubtitle", jdbcType=JdbcType.VARCHAR),
+            @Result(column="blog_number", property="blogNumber", jdbcType=JdbcType.VARCHAR),
+            //@Result(column="blog_content", property="blogContent", jdbcType=JdbcType.LONGVARCHAR)
+    })
+    Page<Blog> getBlogListByPage(Blog blog);
 
     @UpdateProvider(type=BlogSqlProvider.class, method="updateByPrimaryKeySelective")
     int updateByPrimaryKeySelective(Blog record);
 
     @Update({
-        "update T_DAISY_BLOG",
-        "set blog_title = #{blogTitle,jdbcType=VARCHAR},",
-          "blog_author = #{blogAuthor,jdbcType=VARCHAR},",
-          "blog_publishtime = #{blogPublishtime,jdbcType=DATE},",
-          "blog_bannerurl = #{blogBannerurl,jdbcType=VARCHAR},",
-          "blog_introduction = #{blogIntroduction,jdbcType=VARCHAR},",
-          "blog_status = #{blogStatus,jdbcType=VARCHAR},",
-          "blog_type = #{blogType,jdbcType=VARCHAR},",
-          "blog_subtitle = #{blogSubtitle,jdbcType=VARCHAR},",
-          "blog_number = #{blogNumber,jdbcType=VARCHAR},",
-          "blog_content = #{blogContent,jdbcType=LONGVARCHAR}",
-        "where blog_id = #{blogId,jdbcType=INTEGER}"
+            "update T_DAISY_BLOG",
+            "set blog_title = #{blogTitle,jdbcType=VARCHAR},",
+            "blog_author = #{blogAuthor,jdbcType=VARCHAR},",
+            "blog_publishtime = #{blogPublishtime,jdbcType=DATE},",
+            "blog_bannerurl = #{blogBannerurl,jdbcType=VARCHAR},",
+            "blog_introduction = #{blogIntroduction,jdbcType=VARCHAR},",
+            "blog_status = #{blogStatus,jdbcType=VARCHAR},",
+            "blog_type = #{blogType,jdbcType=VARCHAR},",
+            "blog_subtitle = #{blogSubtitle,jdbcType=VARCHAR},",
+            "blog_number = #{blogNumber,jdbcType=VARCHAR},",
+            "blog_content = #{blogContent,jdbcType=LONGVARCHAR}",
+            "where blog_id = #{blogId,jdbcType=INTEGER}"
     })
     int updateByPrimaryKeyWithBLOBs(Blog record);
 
     @Update({
-        "update T_DAISY_BLOG",
-        "set blog_title = #{blogTitle,jdbcType=VARCHAR},",
-          "blog_author = #{blogAuthor,jdbcType=VARCHAR},",
-          "blog_publishtime = #{blogPublishtime,jdbcType=DATE},",
-          "blog_bannerurl = #{blogBannerurl,jdbcType=VARCHAR},",
-          "blog_introduction = #{blogIntroduction,jdbcType=VARCHAR},",
-          "blog_status = #{blogStatus,jdbcType=VARCHAR},",
-          "blog_type = #{blogType,jdbcType=VARCHAR},",
-          "blog_subtitle = #{blogSubtitle,jdbcType=VARCHAR},",
-          "blog_number = #{blogNumber,jdbcType=VARCHAR}",
-        "where blog_id = #{blogId,jdbcType=INTEGER}"
+            "update T_DAISY_BLOG",
+            "set blog_title = #{blogTitle,jdbcType=VARCHAR},",
+            "blog_author = #{blogAuthor,jdbcType=VARCHAR},",
+            "blog_publishtime = #{blogPublishtime,jdbcType=DATE},",
+            "blog_bannerurl = #{blogBannerurl,jdbcType=VARCHAR},",
+            "blog_introduction = #{blogIntroduction,jdbcType=VARCHAR},",
+            "blog_status = #{blogStatus,jdbcType=VARCHAR},",
+            "blog_type = #{blogType,jdbcType=VARCHAR},",
+            "blog_subtitle = #{blogSubtitle,jdbcType=VARCHAR},",
+            "blog_number = #{blogNumber,jdbcType=VARCHAR}",
+            "where blog_id = #{blogId,jdbcType=INTEGER}"
     })
     int updateByPrimaryKey(Blog record);
 }
